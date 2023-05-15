8d43b54ba3a93a8229cdd5eafc860677f713692d
Crowdbotics <team@crowdbotics.com>
2023-05-15 16:58:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fitko-do-kapsy-40705",
+  url: "https://fitko-do-kapsy-40705.botics.co",
+  api: "https://fitko-do-kapsy-40705.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
